0fd32b4a1d780c36f7120c0ac9c41572adfd5ef8
dmytrozhmur <dmytro.zhmur@nure.ua>
2022-12-20 21:21:05 +0200

    Comments taken into account

--- Music Advisor/task/src/advisor/controller/commands/AuthorizationCommand.java
+++ Music Advisor/task/src/advisor/controller/commands/AuthorizationCommand.java
@@ -1,11 +1,9 @@
 package advisor.controller.commands;
 
+import advisor.controller.utils.HttpServerWrapper;
 import advisor.model.DataSource;
 import advisor.view.View;
-import com.sun.net.httpserver.HttpServer;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.util.List;
 
 import com.google.gson.*;
@@ -13,42 +11,29 @@ import com.google.gson.*;
 import static advisor.controller.utils.HttpHandler.*;
 
 public class AuthorizationCommand implements Command {
-    private String code;
 
     @Override
     public boolean execute(final String address, final DataSource source) {
         String accessToken;
-        HttpServer server;
-
+        HttpServerWrapper serverWrapper = new HttpServerWrapper();
         View.otherInform(String.format(ACCESS_LINK, address, PORT));
-        try {
-            server = createServer();
-        } catch (IOException ioe) {
-            View.otherInform("Creating listening server denied");
-            return false;
-        }
+        serverWrapper.waitForCode();
 
-        server.start();
-        View.otherInform("waiting for code...");
-        for (int i = 0; i < 10000; i++) {
-            if(gotCode()) break;
-            else {
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    return false;
-                }
-            }
-        }
+        accessToken = getToken(address, serverWrapper);
+        if (accessToken == null) return false;
+        source.setAccessToken(accessToken);
+        source.setRequestedContent(List.of(new String[]{"Success!"}));
+        return true;
+    }
 
-        server.stop(10);
-        if(!gotCode()) {
+    private String getToken(String address, HttpServerWrapper serverWrapper) {
+        String accessToken;
+        if(!serverWrapper.hasCode()) {
             View.otherInform("no code received");
-            return false;
+            return null;
         }
-        code = code.replace("code=", "");
+        String code = serverWrapper.getCode().replace("code=", "");
         View.otherInform("code received\nmaking http request for access_token...");
-
         try {
             String response = doPOSTRequest(address, code);
             accessToken = JsonParser.parseString(response).getAsJsonObject()
@@ -56,38 +41,9 @@ public class AuthorizationCommand implements Command {
                     .getAsString();
         } catch (Exception e) {
             View.otherInform("Access token not available");
-            return false;
+            return null;
         }
 
-        source.setAccessToken(accessToken);
-        source.setRequestedContent(List.of(new String[]{"Success!"}));
-
-        return true;
+        return accessToken;
     }
-
-    private boolean gotCode() {
-        return code != null && !code.equals("");
-    }
-
-    private HttpServer createServer() throws IOException {
-        HttpServer server = HttpServer.create();
-        server.bind(new InetSocketAddress(PORT), 0);
-        server.createContext("/", exchange -> {
-            while (code == null) code = exchange.getRequestURI().getQuery();
-
-            String body;
-            if(code.startsWith("code")) {
-                body = "Got the code. Return back to your program.";
-            } else {
-                body = "Authorization code not found. Try again.";
-            }
-
-            exchange.sendResponseHeaders(200, body.length());
-            exchange.getResponseBody().write(body.getBytes());
-            exchange.getResponseBody().close();
-        });
-
-        return server;
-    }
-
 }
